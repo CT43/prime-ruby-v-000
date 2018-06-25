d4ae9ab0b8c5d5413361f145e2a9c3238b377815
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-25 18:56:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,15 @@
 require 'pry'
 
 def prime?(n)
-  if n < 0 || n == 0 || n == 1 
+  if n < 0 || n == 0 || n == 1
     return false
   end
   counter = 2
   while n < counter
     if n % counter
       return true
-    else 
+    else
       counter += 1
     end
+  end
 end
