5fb69fd72ee71aa433693faf12984afcb35b7be0
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-25 19:13:52 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,20 +1,11 @@
 require 'pry'
 
 def prime?(n)
-  counter = 1
   if n < 0 || n == 0 || n == 1
     return false
-  elsif n == 2
-    return true 
   else
-    until counter = n
-      if n % counter == 0
-        return false
-      elsif counter == n-1
-        return true
-      else
-        counter += 1
-      end
+    (2...n-1).to_a.each |factor| do
+      n % factor == 0 ? false : true
     end
   end
 end
