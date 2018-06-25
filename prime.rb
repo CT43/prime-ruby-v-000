b7e574de0cc2ceb18c770328e8d43d876dad2909
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-25 19:17:12 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(n)
   if n < 0 || n == 0 || n == 1
     return false
   else
-    (2...n-1).to_a.each do |factor| 
+    (2...n-1).to_a.each do |factor|
       n % factor == 0 ? return false : true
     end
   end
