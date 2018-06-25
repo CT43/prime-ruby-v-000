7a75bec340a22d151039fa3a2fc026d7672f796f
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-25 18:44:03 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 require 'pry'
 
 def prime?(n)
-  counter = 2
+  counter = 1
   while counter < n
+    
     if n % counter == 0
       return false
     else
       counter += 1
-      binding.pry
     end
     return true
   end
